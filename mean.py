80f6f0d4603ed41ec247c145aef26243d280a25d
ayyvedyaa <ayyvedyaa@gmail.com>
2025-11-12 20:52:58 +0530

    Create mean.py

---
+++ mean.py
@@ -0,0 +1,7 @@
+arr = [10, 20, 30, 40, 50]
+total = 0
+for num in arr:
+    total += num
+mean = total / len(arr)
+print("mean is : ", mean)
+#average of all values , formula - sum of all values/total no. of values
