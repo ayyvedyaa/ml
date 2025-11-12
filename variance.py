2aaf501b9d23c3c44783f46878824cfe22814729
ayyvedyaa <ayyvedyaa@gmail.com>
2025-11-12 20:35:42 +0530

    Create variance.py

---
+++ variance.py
@@ -0,0 +1,5 @@
+import numpy as np
+data=[10,20,30,40,50]
+variance=np.var(data)
+print("variance is ", variance)
+#variance measures how far a set of numbers is spread out from their average, measures in square units, variance+ std dev^2
