cd8e89ff8788ce1c638a9cdad6bdebfcdcec5aeb
ayyvedyaa <ayyvedyaa@gmail.com>
2025-11-12 20:54:35 +0530

    Create percentile.py

---
+++ percentile.py
@@ -0,0 +1,5 @@
+import numpy as np
+data=[10,20,30,40,50]
+p25=np.percentile(data,25)
+print("25th perentile:", p25)
+# it indicates the value below which a given percentage of observations in a group of observations falls, tells you how a particular value compares to the rest of the data.
