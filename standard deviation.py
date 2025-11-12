ed3f064d7586503753fdc563dbebdd48b5d25db1
ayyvedyaa <ayyvedyaa@gmail.com>
2025-11-12 20:36:25 +0530

    Create standard deviation.py

---
+++ standard deviation.py
@@ -0,0 +1,5 @@
+import numpy as np
+data=[10,20,30,40,50]
+std_dev=np.std(data)
+print("standard deviation is", std_dev)
+#standard deviation is a number that tells you how spread out the values in a data set are from their average, measures in original units, variance= std dev^2
